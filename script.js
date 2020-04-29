769d045a6b44ca846b04e4e1b7208f9b2a3f32d4
brendon6799 <61213371+brendon6799@users.noreply.github.com>
2020-04-29 20:31:38 -0300

    Add files via upload
    Aluno: Brendon Andrew Lopes Lucena
    RA: 1901959

--- script.js
+++ script.js
@@ -1,25 +1,91 @@
+let btn = document.getElementsByTagName('button')
 
-let btn_operador = ''
-let valor = []
+function set_content(){
+    document.getElementById('resultado').value += btn_click
+}
 
-function operador(btn_operador){
+btn[0].onclick = function(){
+    btn_click = btn[0].innerHTML; set_content()
+}
+
+btn[1].onclick = function(){
+    btn_click = btn[1].innerHTML; set_content()
+}
+
+btn[2].onclick = function(){
+    btn_click = btn[2].innerHTML; set_content()
+}
+//botão soma
+btn[3].onclick = function(){
+    btn_click = btn[3].innerHTML; operador()
+}
+
+btn[4].onclick = function(){
+    btn_click = btn[4].innerHTML; set_content()
+}
+
+btn[5].onclick = function(){
+    btn_click = btn[5].innerHTML; set_content()
+}
+
+btn[6].onclick = function(){
+    btn_click = btn[6].innerHTML; set_content()
+}
+
+btn[7].onclick = function(){
+    btn_click = btn[7].innerHTML; operador()
+}
+
+btn[8].onclick = function(){
+    btn_click = btn[8].innerHTML; set_content()
+}
+
+btn[9].onclick = function(){
+    btn_click = btn[9].innerHTML; set_content()
+}
+
+btn[10].onclick = function(){
+    btn_click = btn[10].innerHTML; set_content()
+}
+
+btn[11].onclick = function(){
+    btn_click = btn[11].innerHTML; operador()
+}
+
+btn[12].onclick = function(){
+    btn_click = btn[12].innerHTML; set_content()
+}
+
+btn[13].onclick = function(){
+    btn_click = btn[13].innerHTML; dot()
+}
+
+btn[14].onclick = function(){
+    btn_click = btn[14].innerHTML; calcular() 
+}
+
+btn[15].onclick = function(){
+    btn_click = btn[15].innerHTML; operador()
+}
+
+function operador(){
+    op = btn_click
     content = document.getElementById('resultado').value
-    if (content == ''){
-        return document.getElementById('resultado').value = '' //Corrigido
+    if(content == ''){
+        return document.getElementById('resultado').value = ''
     }
-    else {
-        document.getElementById('resultado').value += btn_operador
+    if(isNaN(content)){
+        return document.getElementById('resultado').value = ('erro operador duplo')
+    }
+    else{
+        return set_content()
     }
 }
 
-function split_op(btn_operador){
+function split_op(){
     content = document.getElementById('resultado').value
-    if(btn_operador != ''){
-        valor = content.split(btn_operador)
-    }else
-        {valor = content}
+    num = content.split(op)
 }
-
 
 function dot(){
     content = document.getElementById('resultado').value
@@ -32,124 +98,51 @@ function dot(){
     else if(content.split('.').length == 1){
         return set_content()
     }
+    else if(content.split('.').length == 1){
+        return set_content()
+    }
     else{
-        if(btn_operador !=''){
-            split_op()
-            if(valor[0].split('.').length == 2){
-                return set_content()
-            }
+        split_op()
+        if(num[1].split('.').length == 1){
+            return set_content()
         }
-        
     }
 }
 
 
 function calcular(){
-    if(content == '' || valor[1] ==''){
-        return result = 'Erro de operação'
+    content = document.getElementById('resultado').value
+    if(content == ''){
+        document.getElementById('resultado').value = 'Erro de operação'
     }
-    else if(btn_operador == ''){
-        return result = valor
-    }
+    //else if(content.split('.').length ==1){
+        //return parseFloat(document.getElementById('resultado').value)
+    //
     else {
-        if(btn_operador == '+'){
-            return result = parseFloat(valor[0]) + parseFloat(valor[1])
+        if(op == '+'){
+            split_op()
+            result = parseFloat(num[0]) + parseFloat(num[1])
+            document.getElementById('resultado').value = result
         }
-        if(btn_operador == '-'){
-            return result = parseFloat(valor[0]) - parseFloat(valor[1])
+        if(op == '-'){
+            split_op()
+            result = parseFloat(num[0]) - parseFloat(num[1])
+            document.getElementById('resultado').value = result
         }
-        if(btn_operador == '*'){
-            return result = parseFloat(valor[0]) * parseFloat(valor[1])
+        if(op == '*'){
+            split_op()
+            result = parseFloat(num[0]) * parseFloat(num[1])
+            document.getElementById('resultado').value = result
         }
-        if(btn_operador == '/'){
-            return result = parseFloat(valor[0]) / parseFloat(valor[1])
+        if(op == '/'){
+            split_op()
+            result = parseFloat(num[0]) / parseFloat(num[1])
+            document.getElementById('resultado').value = result
         }
     }
-}
+    
+}/*
+*/
 
-function set_content(){
-    document.getElementById('resultado').value += btn_click
-}
 
-let btn = document.getElementsByTagName('button')
 
-btn[0].onclick = function(){
-    btn_click = btn[0].innerHTML;
-    set_content()
-}
-
-btn[1].onclick = function(){
-    btn_click = btn[1].innerHTML;
-    set_content()
-}
-
-btn[2].onclick = function(){
-    btn_click = btn[2].innerHTML;
-    set_content()
-}
-//botão soma
-btn[3].onclick = function(){
-    btn_operador = btn[3].innerHTML;
-    operador(btn_operador)
-}
-
-btn[4].onclick = function(){
-    btn_click = btn[4].innerHTML;
-    set_content()
-}
-
-btn[5].onclick = function(){
-    btn_click = btn[5].innerHTML;
-    set_content()
-}
-
-btn[6].onclick = function(){
-    btn_click = btn[6].innerHTML;
-    set_content()
-}
-
-btn[7].onclick = function(){
-    btn_operador = btn[7].innerHTML;
-    operador(btn_operador)
-}
-
-btn[8].onclick = function(){
-    btn_click = btn[8].innerHTML;
-    set_content()
-}
-
-btn[9].onclick = function(){
-    btn_click = btn[9].innerHTML;
-    set_content()
-}
-
-btn[10].onclick = function(){
-    btn_click = btn[10].innerHTML;
-    set_content()
-}
-
-btn[11].onclick = function(){
-    btn_operador = btn[11].innerHTML;
-    operador(btn_operador)
-}
-
-btn[12].onclick = function(){
-    btn_click = btn[12].innerHTML;
-    set_content()
-}
-
-btn[13].onclick = function(){
-    btn_click = btn[13].innerHTML; dot()
-}
-
-btn[14].onclick = function(){
-    btn_click = btn[14].innerHTML;
-    split_op(btn_operador);
-    calcular();
-    document.getElementById('resultado').value = result
-}
-
-btn[15].onclick = function(){
-    btn_operador = btn[15].innerHTML;
-    operador(btn_operador)
-}
