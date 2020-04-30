2c08c8d61419a1a941ef2b3f8368214e6501393a
brendon6799 <61213371+brendon6799@users.noreply.github.com>
2020-04-29 22:04:24 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,148 +1,100 @@
-let btn = document.getElementsByTagName('button')
+// AC3 Gleitho Suelvillis Moura - RA: 1902351
 
-function set_content(){
-    document.getElementById('resultado').value += btn_click
-}
+let btn_lista = document.getElementsByClassName('numero')
+let content = document.getElementById('resultado')
+let btn_click = ''
+let num1 = ''
+let num2 = ''
+let btn_op = ''
 
-btn[0].onclick = function(){
-    btn_click = btn[0].innerHTML; set_content()
-}
-
-btn[1].onclick = function(){
-    btn_click = btn[1].innerHTML; set_content()
-}
-
-btn[2].onclick = function(){
-    btn_click = btn[2].innerHTML; set_content()
-}
-//botão soma
-btn[3].onclick = function(){
-    btn_click = btn[3].innerHTML; operador()
-}
-
-btn[4].onclick = function(){
-    btn_click = btn[4].innerHTML; set_content()
-}
-
-btn[5].onclick = function(){
-    btn_click = btn[5].innerHTML; set_content()
-}
-
-btn[6].onclick = function(){
-    btn_click = btn[6].innerHTML; set_content()
-}
-
-btn[7].onclick = function(){
-    btn_click = btn[7].innerHTML; operador()
-}
-
-btn[8].onclick = function(){
-    btn_click = btn[8].innerHTML; set_content()
-}
-
-btn[9].onclick = function(){
-    btn_click = btn[9].innerHTML; set_content()
-}
-
-btn[10].onclick = function(){
-    btn_click = btn[10].innerHTML; set_content()
-}
-
-btn[11].onclick = function(){
-    btn_click = btn[11].innerHTML; operador()
-}
-
-btn[12].onclick = function(){
-    btn_click = btn[12].innerHTML; set_content()
-}
-
-btn[13].onclick = function(){
-    btn_click = btn[13].innerHTML; dot()
-}
-
-btn[14].onclick = function(){
-    btn_click = btn[14].innerHTML; calcular() 
-}
-
-btn[15].onclick = function(){
-    btn_click = btn[15].innerHTML; operador()
-}
-
-function operador(){
-    op = btn_click
-    content = document.getElementById('resultado').value
-    if(content == ''){
-        return document.getElementById('resultado').value = ''
-    }
-    if(isNaN(content)){
-        return document.getElementById('resultado').value = ('erro operador duplo')
-    }
-    else{
-        return set_content()
+for(let i = 0; i < btn_lista.length; i++){
+    btn_lista[i].onclick = function(){
+        btn_click = btn_lista[i].innerText;
+        inserir_numero(btn_click);
     }
 }
 
-function split_op(){
-    content = document.getElementById('resultado').value
-    num = content.split(op)
-}
 
-function dot(){
-    content = document.getElementById('resultado').value
-    if(content == ''){
-        return set_content()
-    }
-    else if(content == '.' ){
-        return console.log('erro ja existe um ponto')
-    }
-    else if(content.split('.').length == 1){
-        return set_content()
-    }
-    else if(content.split('.').length == 1){
-        return set_content()
-    }
-    else{
-        split_op()
-        if(num[1].split('.').length == 1){
-            return set_content()
+function inserir_numero(btn_click){
+    if(btn_op == ''){
+        if(num1 == '' && btn_click == '.' ){
+            num1 += btn_click
+            return content.value += btn_click
+        }else if(btn_click != '.' || num1.indexOf('.') == '-1'){
+            num1 += btn_click    
+            return content.value += btn_click
         }
     }
+    if(btn_op != ''){
+        if(num2 == '' && btn_click == '.'){
+            num2 += btn_click
+            return content.value += btn_click
+        }else if(btn_click != '.' || num2.indexOf('.') == '-1'){
+            num2 += btn_click
+            return content.value += btn_click
+        }
+    }
+}
+
+
+function operador(op){
+    if (content.value == ''){
+        alert('imposible')
+    }else if(content.value.indexOf(op.innerText) == '-1' && btn_op == ''){
+        btn_op = op.innerText;
+        content.value += btn_op
+    }
+}
+
+
+let soma = document.getElementsByClassName('operacao somar')
+soma[0].onclick = function(){
+        operador(soma[0])
+}
+
+let subtrair = document.getElementsByClassName('operacao subtrair')
+subtrair[0].onclick = function(){
+    operador(subtrair[0])
+}
+
+let multiplicar = document.getElementsByClassName('operacao multiplicar')
+multiplicar[0].onclick = function(){
+    operador(multiplicar[0])
+}
+
+let dividir = document.getElementsByClassName('operacao dividir')
+dividir[0].onclick = function(){
+    operador(dividir[0])
+}
+
+
+let resultar = document.getElementsByClassName('resultar')
+resultar[0].onclick = function(){
+    calcular()
 }
 
 
 function calcular(){
-    content = document.getElementById('resultado').value
-    if(content == ''){
-        document.getElementById('resultado').value = 'Erro de operação'
+    if(content.value == ''){
+        return alert('Erro de operação')
     }
-    //else if(content.split('.').length ==1){
-        //return parseFloat(document.getElementById('resultado').value)
-    //
-    else {
-        if(op == '+'){
-            split_op()
-            result = parseFloat(num[0]) + parseFloat(num[1])
-            document.getElementById('resultado').value = result
-        }
-        if(op == '-'){
-            split_op()
-            result = parseFloat(num[0]) - parseFloat(num[1])
-            document.getElementById('resultado').value = result
-        }
-        if(op == '*'){
-            split_op()
-            result = parseFloat(num[0]) * parseFloat(num[1])
-            document.getElementById('resultado').value = result
-        }
-        if(op == '/'){
-            split_op()
-            result = parseFloat(num[0]) / parseFloat(num[1])
-            document.getElementById('resultado').value = result
-        }
+    else if (content.value != ''){
+        content.value = num1
     }
-    
-}/*
-*/
-
-
-
+    if(btn_op == '+'){
+        result = parseFloat(num1) + parseFloat(num2)
+        content.value = result
+    }
+    if(btn_op == '-'){
+        result = parseFloat(num1) - parseFloat(num2)
+        content.value = result
+    }
+    if(btn_op == '*'){
+        result = parseFloat(num1) * parseFloat(num2)
+        content.value = result
+    }
+    if(btn_op == '/'){
+        result = parseFloat(num1) / parseFloat(num2)
+        content.value = result
+    }
+}
